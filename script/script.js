1a26e6a92ebf61bb09ec8fb860a433269b28da2b
Александр Кравец <ivanonic488@gmail.com>
2024-11-20 12:27:25 +0200

    added js 20.11.2024

--- script/script.js
+++ script/script.js
@@ -1,20 +1,468 @@
-console.log('Aleksandr');
-console.log(typeof Aleksandr);
-console.log('Aleksandr');
-console.log(typeof Aleksandr);
+
+// const log = ('Aleksandr')
+
+// console.log('log --->', log);
+// console.log(typeof log);
+
+// console.log('alex');
+
+// 'abc' //string
+// 10 //number/
+// 5 + 2
+// c = 10
+// 'good ' + 'evening'
+// a <= b || c !== d
+// myFunction(c, d);
+
+// let a = 10
+// a = 20
+// let b
+// b = false
+// alert(a, b)
+
+
+// const c = 10
+// c = 20 
+// console.log('c --->', c);
+// let myName
+// console.log(myName);
+// console.log(typeof myName);
+
+
+// myName = 'alex'
+// alert(myName)
+// console.log('myName --->', myName);
+// console.log(myName);
+// console.log(typeof myName);
+// console.log(console.log(10));
+
+
+// const myName = 'Alex'
+
+// myName = ''
+
+// console.log(myName);
+
+// const a = 10
+// alert(a)
+
+// const b = 'abc'
+// console.log(a, b);
+// console.log(typeof a, b);
+
+// myName = ('Alex')
+// console.log('myName --->', myName);
+
+
+// const myName = 'Alex'
+// myName = ' '
+// console.log(myName);
+
+// 5 переменных
+
+// -1
+// string  -  text
+// boolean - true is false
+// number - 10, 23, 15
+// null - '  ', " "
+// undefined - undefined
+// symbol - !,?,<,>,=,&
+
+// 1 тип силка
+// objeck - (c, wer, 23,)
+
+// const objectA = {
+//     a: 10,
+//     b: true,
+// }
+// const copyOfA = objectA
+// copyOfA.a = 20
+
+// copyOfA.c = 'abc'
+
+// console.log(objectA, copyOfA, copyOfA.c);
+// console.log(typeof objectA, copyOfA, copyOfA.c);
+// console.log(objectA);
+// console.log(typeof objectA);
+// alert(objectA)
+// alert(typeof objectA)
+
+// String a = 'abc'
+//  a = 'abc'
+//  console.log('a --->', a);
+//  console.log(typeof 'a --->', a);
+//  a = 10
+//  console.log(a);
+//  console.log(typeof a);
+ 
+// int b = 10
+// b = 'xyz' //Error
+
+// a = 'abc'
+// a = 10
+// alert (a)
+
+// let a = 10
+// a = true
+
+// a = 'Alex'
+// a = undefined
+// console.log(a);
+
+// function a() {
+// console.log('Hey there');
+
+// }
+// a()
+// a = 10
+// a()
+
+// const a = () => {
+//     console.log('Hey there-1');
+    
+// }
+// a()
+
+
+// const myArray = [1, 2, 3]
+// console.log(myArray);
+// console.log(typeof  myArray);
+
+
+// const myArray2 = new Array (1, 2, 3)
+// console.log(myArray2);
+// console.log(typeof myArray2);
+
+// function myFn(a) {
+//     console.log(a);
+// }
+
+// const b = true;
+// let c = 10;
+
+// myFn(2 + 3)
+// myFn(b)
+// myFn(c = c + 1)
+// myFn(c = c + 1;)
+// myFn(let d)
+
+
+
+// const myArray = [1, 2, 3]
+// console.log(myArray);
+// console.log(typeof myArray);
+
+
+
+// const myArray2 = new Array(1, 2, 3)
+// console.log(myArray2);
+// console.log(typeof myArray2);
+
+// const myArray = [1, 2, 3]
+// console.log('myArray ---', myArray);
+// console.log(typeof myArray);
+
+// const myObject = {
+//     0: 1,
+//     1: 2,
+//     2: 3,
+//     length:3
+// }
+// console.log('myObject ---', myObject);
+// console.log(typeof myObject);
+
+// const myArray = [1, true, 'a']
+// console.log('myArray ---<', myArray);
+// console.log('myArray ---<', myArray[0]);
+// console.log('myarray --->', myArray[1]);
+// console.log('myArra --->', myArray[3]);
+// console.log('myArray ---', myArray.length);
+
+
+// const myArray = [1, 2, 3, 4]
+// console.log(myArray);
+// console.log(myArray.length);
+
+// myArray[2] = 'abc'
+
+// console.log(myArray);
+// console.log(myArray[2]);
+
+// myArray[4] = true
+// console.log(myArray);
+// console.log(myArray.length);
+
+// myArray[5] = 'Aleksandr'
+// console.log('myArray --->', myArray);
+// console.log('myArray --->', myArray.length);
+// console.log(typeof myArray);
+
+
+
+// const myArray = [1, 2, 3]
+// console.log(myArray);
+
+// myArray.push(4)
+
+// console.log(myArray);
+
+// myArray.push(true)
+
+// console.log(myArray);
+
+// myArray.pop()
+// console.log(myArray);
+
+// const removedElement = myArray.pop()
+
+// console.log(myArray);
+// console.log(removedElement);
+
+// const userProfile = {
+//     name: 'Aleksandr',
+//     commentsQty: 41,
+//     hasSignedAgreement: false,
+// }
+
+// const userInfo = ({ name, commentsQty }) => {
+//     if (!commentsQty) {
+//         return `User ${name} has no comments`
+//     }
+//     return `User ${name} has ${commentsQty} comments`
+// }
+
+// userInfo(userProfile)
+
+// const myFunction(c, d) {
+//     c = 3,
+//     d = 6,
+// }
+// console.log(myFunction);
+
+
+// console.log(`Test!`);
+// console.log(typeof Test );
+
+// var number = 1
+
+
+let number1 = 1
+const number3 = 3.14
+
+console.log(number1);
+
+number1 = 111
+
+
+console.log(number1);
+
+console.log(number3);
+
+
+a = 20
+console.log(a);
+
+a1 = 5
+
+console.log(a1,);
+
+b = 5
+
+b++
+
+console.log(b);
+
+// function myFunction (c, b) {
+//     c = 3,
+//     b = 7,
+//     a3 = c + b
+// }
+
+// console.log(a3);
+
+// let myName = 41
+
+// myName = 42
+
+// console.log(myName);
+
+// let b 
+
+// b = false
+
+// const myName = 41
+
+// myName = 42
+
+let myName
+
+console.log(myName)
+
+myName = `Aleksandr`
+
+console.log(myName)
+
+console.log(console.log(10));
+
+const beby = 10
+
+console.log(beby);
+console.log(typeof beby);
+
+const myNameBeby = `Aleksandr`
+
+console.log(myNameBeby);
+console.log(typeof myNameBeby);
+
+const world = `Hello ` + `World`
+console.log(`world --->`, world);
+console.log(typeof world);
+
+ const testA = {
+    a: 10,
+    b: true
+ }
+ console.log(testA);
+ console.log(typeof testA);
+ 
+const copyOfA = testA
+
+copyOfA.a = 20
+
+console.log(copyOfA.a);
+
+
+ const testA1 = {
+    a: 10,
+    b: true
+ }
+ console.log(testA1);
+ console.log(typeof testA1);
+ 
+const copyOfA1 = testA1
+
+copyOfA.c = `abc`
+
+console.log(copyOfA.c);
+console.log(typeof copyOfA.c);
+
+function myAge() {
+  const year2023 = 41
+  const year2024 = 1
+  sum = c = year2023 + year2024
+  
+  console.log(c);
+  console.log(typeof c);
+}
+myAge()
+console.log(typeof myAge);
+
+const apple = 80 + (`<---kg`)
+console.log(apple);
+console.log(typeof apple);
+
+const apple1 = 80 === (`kg`)
+
+console.log(apple1);
+
+const apple2 = 80 != (`kg`) 
+
+console.log(apple2);
+console.log(typeof apple2);
+
+const apple3 = 80
+const apple4 = (`kg`)
+
+sum = apple5 = 80 + (` kg`)
+
+console.log(apple5);
+console.log(`apple3 --->`, apple3);
+console.log(typeof apple3);
+console.log(`apple4 --->`, apple4);
+console.log(typeof apple4);
+
+const value = 3.14
+
+console.log(`value --->`, value);
+console.log(typeof value);
+
+
+
+const bebyName = `Aleksandr`
+
+console.log(`bebyName --->`, bebyName);
+console.log(typeof bebyName);
+
+const email = `ivanonic488@gmail.com`
+
+console.log(email);
+console.log(typeof email);
+
+const password = `987654321test`
+console.log(password);
+console.log(typeof password);
 
 
 
 
+const test1 = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nemo veniam aspernatur, quisquam eligendi expedita repellendus necessitatibus reiciendis rerum quo id officiis voluptatum, fugiat cupiditate quis ducimus in optio excepturi qui consectetur aliquam est quasi debitis? Quis sint nulla omnis expedita voluptates minima adipisci. Doloribus quaerat eos iure odio architecto sit? Voluptas asperiores officia molestiae, cupiditate possimus animi dolore assumenda minus. Nam voluptate numquam officia incidunt distinctio quasi perferendis animi voluptatem aspernatur suscipit libero veniam assumenda quaerat, atque deserunt repellat odio facilis delectus, asperiores amet tempora ea iusto nobis? Facere, cumque ipsa. Numquam sit expedita quidem modi assumenda quis accusamus.`
+console.log(test1);
+console.log(typeof test1);
 
 
 
+const numString = `10`
+
+console.log(numString);
+console.log(typeof numString);
+
+let isActive = false
+console.log(isActive);
+console.log(typeof isActive);
+
+isActive = true
+console.log(isActive);
+
+// if(input !== true) {
+//    checkbox = off
+// }if else (input === true) {
+//    checkbox = active 
+// }
+
+
+// const myCity = {
+//    city: `New York`
+// }
+// myCity.popular = true
+// console.log(myCity);
+
+// myCity.country = `USA`
+
+// console.log(myCity);
+
+
+// const myCity = {
+//    city: `New York`,
+//    popular: true,
+//    country: `USA`
+// }
+
+// delete myCity.city
+// delete myCity.country
 
 
 
+// console.log(myCity);
 
 
+const myCity = {
+   city: `New York`                     
+}
+
+myCity.city = `Las Vegas`
+
+console.log(myCity);
 
 
-
-
+myCity.country = `USA`
+console.log(myCity);
